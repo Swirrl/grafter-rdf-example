485ffff49e91ae6d7ff894d36e5cc42de339788e
Terpo <nicolas.terpolilli@gmail.com>
2014-08-19 11:18:56 +0100

    make-life-facilitiesv1

--- src/glasgow_life_facilities_2/make_graph.clj
+++ src/glasgow_life_facilities_2/make_graph.clj
@@ -1,7 +1,6 @@
 (ns glasgow-life-facilities-2.make-graph
   (:require [clojure.string :as st]
-            [grafter.tabular :refer :all]
-            [grafter.rdf :refer [prefixer s graph graphify]]
+            [grafter.rdf :refer [graph graph-fn]]
             [grafter.rdf.sesame :as ses]
             [grafter.rdf.ontologies.rdf :refer :all]
             [grafter.rdf.ontologies.void :refer :all]
@@ -15,32 +14,33 @@
             [glasgow-life-facilities-2.pipeline :refer [pipeline]]))
 
 (defn make-life-facilities [path]
-  (let [processed-rows (pipeline path)]
+  (let [dataset (pipeline path)]
 
-         ((graphify [facility-description facility-name monthly-attendence year month address town postcode website facility-type name-slug ref-facility-uri postcode-uri date prefix-date type-name observation-uri]
+    ((graph-fn [{:strs [facility-description facility-name monthly-attendance
+                        year month address town postcode website facility-type
+                        name-slug ref-facility-uri postcode-uri date prefix-date
+                        type-name observation-uri]}]
 
-                    (graph (base-graph "glasgow-life-facilities")
-                          [ref-facility-uri
-                            [rdfs:label (rdfstr name)]
-                            [vcard:hasUrl website]
-                            [rdf:a (urban "Museum")]
-                            [rdf:a (urban "LeisureFacility")]
-                            [vcard:hasAddress [[rdf:a vcard:Address]
-                                              [rdfs:label street-address]
-                                              [vcard:street-address street-address]
-                                              [vcard:locality city]
-                                              [vcard:country-name (rdfstr "Scotland")]
-                                              [vcard:postal-code postcode]
-                                              [os:postcode postcode-uri]]]])
+               (graph (base-graph "glasgow-life-facilities")
+                      [ref-facility-uri
+                       [rdfs:label (rdfstr name)]
+                       [vcard:hasUrl website]
+                       [rdf:a (urban "Museum")]
+                       [rdf:a (urban "LeisureFacility")]
+                       [vcard:hasAddress [[rdf:a vcard:Address]
+                                          [rdfs:label address]
+                                          [vcard:street-address address]
+                                          [vcard:locality town]
+                                          [vcard:country-name (rdfstr "Scotland")]
+                                          [vcard:postal-code postcode]
+                                          [os:postcode postcode-uri]]]])
 
-                    (graph (base-graph "glasgow-life-attendances")
-                           [observation-uri
-                            [(glasgow "refFacility") ref-facility-uri]
-                            [(glasgow "numAttendees") attendance]
-                            [qb:dataSet "http://linked.glasgow.gov.uk/data/glasgow-life-attendances"]
-                            [(sd "refPeriod") "http://reference.data.gov.uk/id/month/2013-09"]
-                            [rdf:a qb:Observation]]))
+               (graph (base-graph "glasgow-life-attendances")
+                      [observation-uri
+                       [(glasgow "refFacility") ref-facility-uri]
+                       [(glasgow "numAttendees") monthly-attendance]
+                       [qb:dataSet "http://linked.glasgow.gov.uk/data/glasgow-life-attendances"]
+                       [(sd "refPeriod") "http://reference.data.gov.uk/id/month/2013-09"]
+                       [rdf:a qb:Observation]]))
 
-          processed-rows)
-         ;processed-rows
-         ))
+     dataset)))
