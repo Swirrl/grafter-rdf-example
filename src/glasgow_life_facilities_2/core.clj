80ebf4ef9ad63987bb2710e7ea26b0bdd530640f
Rick Moynihan <rick.m@swirrl.com>
2014-08-22 16:18:34 +0100

    Output instructions when no args are passed.

--- src/glasgow_life_facilities_2/core.clj
+++ src/glasgow_life_facilities_2/core.clj
@@ -18,12 +18,13 @@
 
     (pr/add (ses/rdf-serializer destination) quads)))
 
-(defn -main [path output]
+(defn -main [& [path output]]
+  (when-not (and path output)
+    (println "Usage: lein run <input-file.csv> <output-file.(nt|rdf|n3|ttl)>")
+    (System/exit 0))
   (-> (open-all-datasets path)
       first
       pipeline
       glasgow-life-facilities-template
       (import-life-facilities output))
   (println path "has been grafted using Grafter 0.2.0!"))
-
-
